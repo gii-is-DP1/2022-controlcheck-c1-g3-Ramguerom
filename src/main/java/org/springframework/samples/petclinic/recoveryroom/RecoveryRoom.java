e4510431cf921b4704efd80825184bac8cb0783d
Ramguerom <ramguerom@alum.us.es>
2022-11-03 17:17:15 +0100

    ENTREGA ramguerom

--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoom.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoom.java
@@ -1,17 +1,42 @@
 package org.springframework.samples.petclinic.recoveryroom;
 
-import javax.persistence.Transient;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class RecoveryRoom {
-    Integer id;
+@Entity
+@Table(name = "recovery_rooms")
+public class RecoveryRoom extends BaseEntity {
+	
+	@Column(name = "name")
+	@NotEmpty
+	@Size(min=3, max= 50)
     String name;
+	
+	@Column(name = "size")
+	@NotNull
+	@PositiveOrZero
     double size;
+	
+	@Column(name = "secure")
+	@NotNull
     boolean secure;
-    @Transient
+	
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "room_type_id")
+	@NotNull
     RecoveryRoomType roomType;
 }
