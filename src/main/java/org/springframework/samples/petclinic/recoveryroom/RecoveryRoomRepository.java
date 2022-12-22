fd94b1017be1649e9aaac5e3b502e489d0a5a3d0
Ramguerom <ramguerom@alum.us.es>
2022-12-22 11:01:39 +0100

    prueba

--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomRepository.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomRepository.java
@@ -3,10 +3,21 @@ package org.springframework.samples.petclinic.recoveryroom;
 import java.util.List;
 import java.util.Optional;
 
-public interface RecoveryRoomRepository {
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecoveryRoomRepository extends CrudRepository<RecoveryRoom, Integer> {
+    
     List<RecoveryRoom> findAll();
     Optional<RecoveryRoom> findById(int id);
     RecoveryRoom save(RecoveryRoom p);
-    //List<RecoveryRoomType> findAllRecoveryRoomTypes();
-    //RecoveryRoomType getRecoveryRoomType(String name);
+    
+    @Query("SELECT recovery_room_type FROM RecoveryRoomType recovery_room_type")
+    List<RecoveryRoomType> findAllRecoveryRoomTypes();
+    
+    @Query("SELECT recovery_room_type FROM RecoveryRoomType recovery_room_type WHERE recovery_room_type.name =:name")
+    RecoveryRoomType getRecoveryRoomType(@Param("name") String name);
 }
--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomService.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomService.java
@@ -2,21 +2,46 @@ package org.springframework.samples.petclinic.recoveryroom;
 
 import java.util.List;
 
-public class RecoveryRoomService {
-    public List<RecoveryRoom> getAll(){
-        return null;
-    }
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.pet.exceptions.DuplicatedPetNameException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class RecoveryRoomService {
+	
+	RecoveryRoomRepository recoveryRoomRepository;
+	
+	@Autowired
+	public RecoveryRoomService(RecoveryRoomRepository recoveryRoomRepository) {
+		this.recoveryRoomRepository = recoveryRoomRepository;
+	}
+	
+	@Transactional(readOnly = true)
+    public List<RecoveryRoom> getAll(){
+        return recoveryRoomRepository.findAll();
+    }
+    
+    @Transactional(readOnly = true)
     public List<RecoveryRoomType> getAllRecoveryRoomTypes(){
         return null;
     }
 
+    @Transactional(readOnly = true)
     public RecoveryRoomType getRecoveryRoomType(String typeName) {
-        return null;
+        return recoveryRoomRepository.getRecoveryRoomType(typeName);
     }
 
+    @Transactional(rollbackFor = DuplicatedRoomNameException.class)
     public RecoveryRoom save(RecoveryRoom p) throws DuplicatedRoomNameException {
-        return null;       
+    	List<RecoveryRoom> rooms = getAll();
+    	for(RecoveryRoom r:rooms) {
+    		if(r.getName().equals(p.getName())) {
+    			throw new DuplicatedRoomNameException();
+    		}
+    	}
+        return recoveryRoomRepository.save(p);       
     }
 
     
